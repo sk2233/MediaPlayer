c80d047e180e564a96cc973d923e96a592682f19
Sk <2171783438@qq.com>
2020-07-29 18:50:27 +0800

    添加底部导航栏

--- app/src/main/java/com/sk/mediaplayer/MainActivity.java
+++ app/src/main/java/com/sk/mediaplayer/MainActivity.java
@@ -3,12 +3,16 @@ package com.sk.mediaplayer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
+
+    RadioGroup radioGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        radioGroup=findViewById(R.id.rg_main);
     }
 }
